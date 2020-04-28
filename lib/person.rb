0bd06471b0d3091253db3bc61bffacdfae1c83e1
aromsa <annacbateman@gmail.com>
2020-04-28 18:28:50 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class Person
-  #your code here
+
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+  :weight, :handed, :complexion, :t_shirt_size, 
+  :wrist_size, :glove_size, :pant_length, :pant_width, :nose_size
+
+  def initialize(attributes)
+    if attributes
+      attributes.each do |k,v|
+        self.send("#{k}=", v)
+        # binding.pry
+      end
+    end
+  end
 end
